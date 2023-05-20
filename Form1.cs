574e19b0ddd87ea4b65c3499460287cfab96c601
GabrielRgz <80359417+GabrielRgz@users.noreply.github.com>
2023-05-19 20:02:36 -0700

    1

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using MongoDB.Bson;
+using MongoDB.Driver;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,34 @@ namespace ABD_Monitoreo_MongoDB
 {
     public partial class Form1 : Form
     {
+        //Variables 
+        string conexion = "cadena de conexion";
+        MongoClient cliente;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                cliente = new MongoClient(conexion);
+                var result = cliente.GetDatabase("admin").RunCommand<BsonDocument>(new BsonDocument("ping", 1));
+                //var bd = cliente.GetDatabase("Super").GetCollection<BsonDocument>("Productos");
+                Console.WriteLine("Conectado correctamente a MongoDB");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
         }
     }
 }
