f67ec8bdedbdac4780b53a0aacff2503f125ae7d
GabrielRgz <80359417+GabrielRgz@users.noreply.github.com>
2023-05-20 21:51:51 -0700

    Muestra Colecciones
    Se cambio los elementos a mostrar en la listbox por las colecciones de la base de datos.
    Se agrego un data grid view para mostrar los dicumentos de la coleccion seleccionada.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,9 +30,12 @@
         {
             this.lblTitulo = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.lbxBasesDatos = new System.Windows.Forms.ListBox();
+            this.lbxColecciones = new System.Windows.Forms.ListBox();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lblColecciones = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // lblTitulo
@@ -42,13 +45,13 @@
             this.lblTitulo.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.lblTitulo.Location = new System.Drawing.Point(2, 7);
             this.lblTitulo.Name = "lblTitulo";
-            this.lblTitulo.Size = new System.Drawing.Size(134, 20);
+            this.lblTitulo.Size = new System.Drawing.Size(137, 20);
             this.lblTitulo.TabIndex = 0;
-            this.lblTitulo.Text = "Bases de datos";
+            this.lblTitulo.Text = "(Base de datos)";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(574, 151);
+            this.button1.Location = new System.Drawing.Point(504, 415);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
@@ -56,19 +59,20 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // lbxBasesDatos
+            // lbxColecciones
             // 
-            this.lbxBasesDatos.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.lbxColecciones.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
-            this.lbxBasesDatos.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.lbxBasesDatos.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbxBasesDatos.FormattingEnabled = true;
-            this.lbxBasesDatos.ItemHeight = 15;
-            this.lbxBasesDatos.Location = new System.Drawing.Point(0, 31);
-            this.lbxBasesDatos.Name = "lbxBasesDatos";
-            this.lbxBasesDatos.Size = new System.Drawing.Size(139, 420);
-            this.lbxBasesDatos.TabIndex = 2;
-            this.lbxBasesDatos.Tag = "Bases de datos";
+            this.lbxColecciones.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lbxColecciones.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbxColecciones.FormattingEnabled = true;
+            this.lbxColecciones.ItemHeight = 15;
+            this.lbxColecciones.Location = new System.Drawing.Point(0, 31);
+            this.lbxColecciones.Name = "lbxColecciones";
+            this.lbxColecciones.Size = new System.Drawing.Size(139, 420);
+            this.lbxColecciones.TabIndex = 2;
+            this.lbxColecciones.Tag = "Bases de datos";
+            this.lbxColecciones.SelectedIndexChanged += new System.EventHandler(this.lbxBasesDatos_SelectedIndexChanged);
             // 
             // panel1
             // 
@@ -79,20 +83,44 @@
             this.panel1.Size = new System.Drawing.Size(139, 34);
             this.panel1.TabIndex = 3;
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(187, 68);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(527, 308);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // lblColecciones
+            // 
+            this.lblColecciones.AutoSize = true;
+            this.lblColecciones.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblColecciones.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblColecciones.Location = new System.Drawing.Point(183, 31);
+            this.lblColecciones.Name = "lblColecciones";
+            this.lblColecciones.Size = new System.Drawing.Size(115, 20);
+            this.lblColecciones.TabIndex = 5;
+            this.lblColecciones.Text = "Documentos:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblColecciones);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.lbxBasesDatos);
+            this.Controls.Add(this.lbxColecciones);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "MonitoringApp";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -100,8 +128,10 @@
 
         private System.Windows.Forms.Label lblTitulo;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.ListBox lbxBasesDatos;
+        private System.Windows.Forms.ListBox lbxColecciones;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lblColecciones;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -23,43 +23,94 @@ namespace ABD_Monitoreo_MongoDB
             InitializeComponent();
         }
 
+        private void MostrarERROR(Exception e) {
+            MessageBox.Show(e.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
         private void Form1_Load(object sender, EventArgs e)
         {
             // Cadena de conexión a tu instancia de MongoDB
-            conexion = "cadena de conexion";
+            conexion = "cadena conexion";
 
             try
             {
                 // Crea el cliente de MongoDB
-                var client = new MongoClient(conexion);
+                cliente = new MongoClient(conexion);
+
+                var database = cliente.GetDatabase("nombre base de datos");
+
+                // Obtiene la lista de colecciones
+                List<string> collectionNames = database.ListCollectionNames().ToList();
 
                 // Obtiene la lista de bases de datos
-                List<string> listaBD = client.ListDatabaseNames().ToList();
+                //List<string> listaBD = cliente.ListDatabaseNames().ToList();
 
                 // Muestra las bases de datos en un ListBox (u otro control de tu elección)
-                foreach (var databaseName in listaBD)
+                foreach (var databaseName in collectionNames)
                 {
-                    lbxBasesDatos.Items.Add(databaseName);
+                    lbxColecciones.Items.Add(databaseName);
                 }
             }
             catch (Exception ex) {
-                MessageBox.Show(ex.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MostrarERROR(ex);
             }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+            /*
             try
             {
                 cliente = new MongoClient(conexion);
                 var result = cliente.GetDatabase("admin").RunCommand<BsonDocument>(new BsonDocument("ping", 1));
-                //var bd = cliente.GetDatabase("Super").GetCollection<BsonDocument>("Productos");
                 Console.WriteLine("Conectado correctamente a MongoDB");
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex);
+                MostrarERROR(ex);
+            }*/
+        }
+
+        private void lbxBasesDatos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+
+                //Obtiene la base de datos
+                var database = cliente.GetDatabase("nombre base de datos");
+                //Obtiene la coleccion
+                var collection = database.GetCollection<BsonDocument>(lbxColecciones.SelectedItem.ToString());
+
+                //Agrega los elementos de la coleccion a una lista
+                var documents = collection.Find(new BsonDocument()).ToList();
+
+                var dataTable = new System.Data.DataTable();
+
+                // Crea las columnas del DataTable
+                foreach (var element in documents.First().Elements)
+                {
+                    dataTable.Columns.Add(element.Name);
+                }
+
+                // Agrega las filas al DataTable
+                foreach (var document in documents)
+                {
+                    var row = dataTable.NewRow();
+
+                    foreach (var element in document.Elements)
+                    {
+                        row[element.Name] = element.Value.ToString();
+                    }
+
+                    dataTable.Rows.Add(row);
+                }
+
+                // Asigna el DataTable como origen de datos para el DataGridView
+                dataGridView1.DataSource = dataTable;
+            }
+            catch (Exception ex)
+            {
+                MostrarERROR(ex);
             }
         }
     }
